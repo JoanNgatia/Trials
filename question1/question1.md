695238ed7684ca8d87996e430597fae288a9f36b
andela-jngatia <joan.ngatia@andela.com>
2016-07-21 18:24:52 +0300

    [Chore] Codebase clean up

---
+++ question1/question1.md
@@ -0,0 +1,17 @@
+##Basic Scripting
+
+- Download this dataset [dataset](https://www.kaggle.com/freecodecamp/2016-new-coder-survey-) from Kaggle.
+- Create a function that reads 100 records from the file per time based on a user supplied start index and a fixed query size of 100.
+- Create a report of the following data per 100 records:
+    - Highest level of education of new coders
+    - Most Popular school major amongst new coders
+    - Percentage of new coders who owe student debt and the average amount of student debt owed
+    - Percentage of new coders that attended coding bootcamps versus the ones who didn’t.
+
+
+####Sample code
+ ` def education_report(start_index):
+        # Read from data file
+        returns [HIGHEST_LEVEL_OF_EDUCATION, MOST_POPULAR_SCHOOL, PER
+
+        CENTAGE_OWING_STUDENT_DEBT, PERCENTAGE_THAT_ATTENDED_CODING_BOOTCAMPS] `
---
+++ question1/readile.py
@@ -0,0 +1,61 @@
+from __future__ import division
+
+
+def education_report(index):
+    """Func- take in index to start and find 100 records from the index.
+
+    Open csv file to access records
+    params:
+    - index - value to start from
+    Find 100 next records from the index.
+    """
+    attended_bootcamp = 0
+    no_bootcamp = 0
+    majors = []
+    school_majors = []
+    education_level = []
+    owe_debt = []
+    indices = int(index) + 10
+    degrees_levels = ["Ph.D.", "master's degree (non-professional)",
+                      "bachelor's degree", "associate\'s degree", " no degree",
+                      "high school diploma or equivalent (GED)",
+                      "some high school",
+                      "no high school (secondary school)",
+                      ]
+    levels = []
+    data = open("data.csv").readlines()
+    for val in range(index, indices):
+        # Retrieve info for each of the coders in lists for each.
+        final_data = data[val].split(",")
+
+        # get highest level of education and compare with defined levels.
+        if final_data[-3] != 'NA':
+            education_level.append(final_data[-3].strip("\"\n"))
+
+        for level in education_level:
+            if level in degrees_levels:
+                levels.append(degrees_levels.index(level))
+
+        # most popular school major value at position[-2]
+        # get tuple of major and no of occurrences
+        if final_data[-2] != 'NA':
+            school_majors.append(final_data[-2])
+        for major in school_majors:
+            (major, school_majors.count(major))
+            majors.append(school_majors.count(major))
+
+        # percentage that owe debt and average amount of debt owed
+        if final_data[-1] != 'NA\n':
+            owe_debt.append(int(final_data[-1].strip("\"\n")))
+
+        # percentage of new that attended bootcamps vs those who didn't,
+        if final_data[1] == '0':
+            no_bootcamp = no_bootcamp + 1
+        else:
+            attended_bootcamp = attended_bootcamp + 1
+
+    print "Percentage that attended bootcamps: {}%".format(attended_bootcamp / no_bootcamp * 100)
+    print "Percentage owing student debt: {}%".format(len(owe_debt) / (val - len(owe_debt)) * 100)
+    print "Average debt owed is: {}".format(sum(owe_debt) / len(owe_debt))
+    print "Most popular school major is: {}".format(school_majors[max(majors)])
+    print "Highest level of educatin is: {}".format(degrees_levels[min(levels)])
