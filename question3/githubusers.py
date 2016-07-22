4433b94ae87d6bbeb82f261eaf2b621b25cd4a6f
andela-jngatia <joan.ngatia@andela.com>
2016-07-22 12:37:29 +0300

    [feature] Model repositories and contributions classes.

--- question3/githubusers.py
+++ question3/githubusers.py
@@ -10,3 +10,21 @@ class User(object):
         self.email = email
         self.username = username
         self.date_joined = date_joined
+
+
+class Repository(object):
+    """Initialize repository class with attributes."""
+
+    def __init__(self, name, date_created, owner):
+        self.name = name
+        self.date_created = date_created
+        self.owner = owner
+
+
+class Contribution(object):
+    """Initialize contributions class with its attributes."""
+
+    def __init__(self, name, repository, creator):
+        self.name = name
+        self.repository = repository
+        self.creator = creator
